8fe21e2cf9031559ef65fbae1972f4f9a119b78a
Denis Uzhva <denis.uzhva@yahoo.com>
2023-06-23 22:46:46 +0300

    exp4-6 ready

--- RunSession.py
+++ RunSession.py
@@ -1,7 +1,8 @@
 import numpy as np
 import warnings
 import yaml
-import argparse
+#import argparse
+import sys
 import itertools
 import os
 import pandas as pd
@@ -24,9 +25,16 @@ def product_dict(**kwargs):
     for instance in itertools.product(*kwargs.values()):
         yield dict(zip(keys, instance))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--config', help="Config name", required=False, default='exprt_1')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser()
+#parser.add_argument('-c', '--config', help="Config name", required=False, default='exprt_1')
+#args = parser.parse_args()
+#configs = args.config.split('&')
+#print(configs)
+
+if len(sys.argv) == 1:
+    configs = ['exprt_1']
+else:
+    configs = sys.argv[1:]
 
 # Initialize obstacles [TODO]
 
@@ -111,99 +119,99 @@ def linear_mpc(
 if __name__ == '__main__':
 
     # Load config
-    config_name = args.config
-    with open(f'cfg/{config_name}.yaml') as f:
-        experiment_parameters = yaml.load(f, Loader=yaml.FullLoader)
-    exprts = list(product_dict(**experiment_parameters))
+    for config_name in configs:
+        with open(f'cfg/{config_name}.yaml') as f:
+            experiment_parameters = yaml.load(f, Loader=yaml.FullLoader)
+        exprts = list(product_dict(**experiment_parameters))
 
-    with open(f'cfg/metaparams.yaml') as f:
-        metaparams = yaml.load(f, Loader=yaml.FullLoader)
-    n_exper_runs = metaparams['n_exper_runs']
-    do_mp = metaparams['multiprocess']
-    do_dynamics = metaparams['do_dynamics']
-    do_statistics = metaparams['do_statistics']
+        with open(f'cfg/metaparams.yaml') as f:
+            metaparams = yaml.load(f, Loader=yaml.FullLoader)
+        n_exper_runs = metaparams['n_exper_runs']
+        do_mp = metaparams['multiprocess']
+        do_dynamics = metaparams['do_dynamics']
+        do_statistics = metaparams['do_statistics']
 
-    if PYPAPI_SPEC is not None:
-        print("OP count enabled")
-    else:
-        print("! OP count not available")
+        if PYPAPI_SPEC is not None:
+            print("OP count enabled")
+        else:
+            print("! OP count not available")
 
-    # Results initialization
-    res_dir = f'results/{config_name}/'
-    os.makedirs(res_dir, exist_ok=True)
-    df_res_path = res_dir + 'statistics.csv'
-    df_dyn_path = res_dir + 'dynamics.csv'
-    df_cost_path = res_dir + 'costs.csv'
+        # Results initialization
+        res_dir = f'results/{config_name}/'
+        os.makedirs(res_dir, exist_ok=True)
+        df_res_path = res_dir + 'statistics.csv'
+        df_dyn_path = res_dir + 'dynamics.csv'
+        df_cost_path = res_dir + 'costs.csv'
 
-    if os.path.exists(df_res_path):
-        df_res_header = False
-    else:
-        df_res_header = True
-
-    exprt_keys = exprts[0].keys()
-    df_res_dict = {key: [] for key in exprt_keys} | {'cvx_time_MEAN': [],
-                                                     'cvx_time_STD': [],
-                                                     'cvx_time_nocoup_MEAN': [],
-                                                     'cvx_time_nocoup_STD': [],
-                                                     'cvx_ops_MEAN': [],
-                                                     'cvx_ops_STD': [],
-                                                     'cvx_ops_nocoup_MEAN': [],
-                                                     'cvx_ops_nocoup_STD': [],
-                                                     'cost_val_MEAN': [],
-                                                     'cost_val_STD': [],
-                                                     'avg_goal_dist_MEAN': [],
-                                                     'avg_goal_dist_STD': [],}
-
-    if do_dynamics:
-        print("DYNAMICS RUN")
-        dyn_exprt_microcoup = {key: val[-1] for key, val in experiment_parameters.items() if key != 'control_strategy'} | {'control_strategy': 'microcoup'}
-        dyn_exprt_mesocoup = {key: val[-1] for key, val in experiment_parameters.items() if key != 'control_strategy'} | {'control_strategy': 'mesocoup'}
-        print(dyn_exprt_microcoup)
-        print(dyn_exprt_mesocoup)
-        np.random.seed(rnd_seed)
-        _, _, _, _, cost_microcoup, dyn_microcoup = linear_mpc(**dyn_exprt_microcoup, dynamics_pic_dir=res_dir)
-        np.random.seed(rnd_seed)
-        _, _, _, _, cost_mesocoup, dyn_mesocoup = linear_mpc(**dyn_exprt_mesocoup, dynamics_pic_dir=res_dir)
-        df_dyn = pd.DataFrame.from_dict({'microcoup': dyn_microcoup, 'mesocoup': dyn_mesocoup})
-        df_dyn.to_csv(df_dyn_path, mode='w', header=True, index=False)
-        df_cost = pd.DataFrame.from_dict({'microcoup': cost_microcoup, 'mesocoup': cost_mesocoup})
-        df_cost.to_csv(df_cost_path, mode='w', header=True, index=False)
-
-    if do_statistics:
-        print("STATISTICS RUN")
-        for exprt in exprts:
-            print(exprt)
-            outs = []
-            np.random.seed(rnd_seed)
-            if do_mp:
-                exprt_list = [exprt for _ in range(n_exper_runs)]
-                outs = mp_kwargs_wrapper(linear_mpc, exprt_list)
-            else:
-                for edx in range(n_exper_runs):
-                    task_out = linear_mpc(**exprt)
-                    outs.append(task_out)
-            outs = np.array([(*out[:-1], out[-1][-1]) for out in outs])
-            out_means = np.mean(outs, axis=0) 
-            out_stds = np.std(outs, axis=0) 
-            for key in exprt_keys: 
-                df_res_dict[key] = [exprt[key]] 
-        
-            df_res_dict['cvx_time_MEAN'] = [out_means[0]]
-            df_res_dict['cvx_time_STD'] = [(out_stds[0])]
-            df_res_dict['cvx_time_nocoup_MEAN'] = [out_means[1]]
-            df_res_dict['cvx_time_nocoup_STD'] = [(out_stds[1])]
-            df_res_dict['cvx_ops_MEAN'] = [out_means[2]]
-            df_res_dict['cvx_ops_STD'] = [(out_stds[2])]
-            df_res_dict['cvx_ops_nocoup_MEAN'] = [out_means[3]]
-            df_res_dict['cvx_ops_nocoup_STD'] = [(out_stds[3])]
-            df_res_dict['cost_val_MEAN'] = [(out_means[4])]
-            df_res_dict['cost_val_STD'] = [(out_stds[4])]
-            df_res_dict['avg_goal_dist_MEAN'] = [(out_means[5])]
-            df_res_dict['avg_goal_dist_STD'] = [(out_stds[5])]
-        
-            df_res = pd.DataFrame.from_dict(df_res_dict)
-            df_res.to_csv(df_res_path, mode='a', header=df_res_header, index=False)
+        if os.path.exists(df_res_path):
             df_res_header = False
-        
+        else:
+            df_res_header = True
 
+        exprt_keys = exprts[0].keys()
+        df_res_dict = {key: [] for key in exprt_keys} | {'cvx_time_MEAN': [],
+                                                        'cvx_time_STD': [],
+                                                        'cvx_time_nocoup_MEAN': [],
+                                                        'cvx_time_nocoup_STD': [],
+                                                        'cvx_ops_MEAN': [],
+                                                        'cvx_ops_STD': [],
+                                                        'cvx_ops_nocoup_MEAN': [],
+                                                        'cvx_ops_nocoup_STD': [],
+                                                        'cost_val_MEAN': [],
+                                                        'cost_val_STD': [],
+                                                        'avg_goal_dist_MEAN': [],
+                                                        'avg_goal_dist_STD': [],}
+
+        if do_dynamics:
+            print("DYNAMICS RUN")
+            dyn_exprt_microcoup = {key: val[-1] for key, val in experiment_parameters.items() if key != 'control_strategy'} | {'control_strategy': 'microcoup'}
+            dyn_exprt_mesocoup = {key: val[-1] for key, val in experiment_parameters.items() if key != 'control_strategy'} | {'control_strategy': 'mesocoup'}
+            print(dyn_exprt_microcoup)
+            print(dyn_exprt_mesocoup)
+            np.random.seed(rnd_seed)
+            _, _, _, _, cost_microcoup, dyn_microcoup = linear_mpc(**dyn_exprt_microcoup, dynamics_pic_dir=res_dir)
+            np.random.seed(rnd_seed)
+            _, _, _, _, cost_mesocoup, dyn_mesocoup = linear_mpc(**dyn_exprt_mesocoup, dynamics_pic_dir=res_dir)
+            df_dyn = pd.DataFrame.from_dict({'microcoup': dyn_microcoup, 'mesocoup': dyn_mesocoup})
+            df_dyn.to_csv(df_dyn_path, mode='w', header=True, index=False)
+            df_cost = pd.DataFrame.from_dict({'microcoup': cost_microcoup, 'mesocoup': cost_mesocoup})
+            df_cost.to_csv(df_cost_path, mode='w', header=True, index=False)
+
+        if do_statistics:
+            print("STATISTICS RUN")
+            for exprt in exprts:
+                print(exprt)
+                outs = []
+                np.random.seed(rnd_seed)
+                if do_mp:
+                    exprt_list = [exprt for _ in range(n_exper_runs)]
+                    outs = mp_kwargs_wrapper(linear_mpc, exprt_list)
+                else:
+                    for edx in range(n_exper_runs):
+                        task_out = linear_mpc(**exprt)
+                        outs.append(task_out)
+                outs = np.array([(*out[:-1], out[-1][-1]) for out in outs])
+                out_means = np.mean(outs, axis=0) 
+                out_stds = np.std(outs, axis=0) 
+                for key in exprt_keys: 
+                    df_res_dict[key] = [exprt[key]] 
             
+                df_res_dict['cvx_time_MEAN'] = [out_means[0]]
+                df_res_dict['cvx_time_STD'] = [(out_stds[0])]
+                df_res_dict['cvx_time_nocoup_MEAN'] = [out_means[1]]
+                df_res_dict['cvx_time_nocoup_STD'] = [(out_stds[1])]
+                df_res_dict['cvx_ops_MEAN'] = [out_means[2]]
+                df_res_dict['cvx_ops_STD'] = [(out_stds[2])]
+                df_res_dict['cvx_ops_nocoup_MEAN'] = [out_means[3]]
+                df_res_dict['cvx_ops_nocoup_STD'] = [(out_stds[3])]
+                df_res_dict['cost_val_MEAN'] = [(out_means[4])]
+                df_res_dict['cost_val_STD'] = [(out_stds[4])]
+                df_res_dict['avg_goal_dist_MEAN'] = [(out_means[5])]
+                df_res_dict['avg_goal_dist_STD'] = [(out_stds[5])]
+            
+                df_res = pd.DataFrame.from_dict(df_res_dict)
+                df_res.to_csv(df_res_path, mode='a', header=df_res_header, index=False)
+                df_res_header = False
+            
+
+                
